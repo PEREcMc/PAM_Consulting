9d8bbae703615db919e63a8d23de639a6ded918e
PEREcMC <perecvlasenko@gmail.com>
2022-09-17 20:04:43 +0300

    added who we are and migration pages

--- js/services-menu.js
+++ js/services-menu.js
@@ -4,3 +4,13 @@ const servicesMenuWrapper = document.querySelector('.services-menu__wrapper');
 linkServicesMenu.addEventListener('click', () => {
     servicesMenuWrapper.classList.toggle('hidden');
 })
+
+// servicesMenuWrapper.addEventListener('click', (e) => {
+//     e.stopPropagation();
+// })
+
+function closeMenu () {
+    window.addEventListener('click', () => {
+      servicesMenuWrapper.classList.add('hidden');
+    })
+}
